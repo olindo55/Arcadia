784a4ee9382ec469368ea10b312c810ca8da63f3
olindo55 <julien.martinati@gmail.com>
2024-08-11 22:01:39 +0100

    remove the carousel

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -2,7 +2,7 @@ import Route from "./Route.js";
 
 //Définir ici les routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/asset/pages/home.html", "/asset/js/home.js"), //"/asset/js/home.js"
+    new Route("/", "Accueil", "/asset/pages/home.html", ), //"/asset/js/home.js"
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
