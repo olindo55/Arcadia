7bc5357bde57bf6466274d09c20eef93f2f7642d
olindo55 <julien.martinati@gmail.com>
2024-08-22 22:28:30 +0100

    add service.html

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -5,6 +5,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/asset/pages/home.html", "/asset/js/home.js"),
     new Route("/contact", "Contact", "/asset/pages/contact.html", "/asset/js/contact.js"),
     new Route("/signin", "Connexion", "/asset/pages/signin.html", "/asset/js/signin.js"),
+    new Route("/service", "Service", "/asset/pages/service.html", "/asset/js/service.js"),
 ];
 
 //The title
