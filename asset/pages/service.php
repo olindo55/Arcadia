5b41e2d360bb69a3e3700c883c22ff3eb4685a72
olindo55 <julien.martinati@gmail.com>
2024-08-28 18:02:11 +0100

    service page  updated with the new DB

--- asset/pages/service.php
+++ asset/pages/service.php
@@ -1,8 +1,28 @@
+<?php include_once 'connection/pdo.php'; ?>
 <section class="service-hero">
     <h1>Nos services<span class="point">.</span></h1>
     <p>Bienvenue au Zoo Arcadia, un lieu dédié à la découverte et à la protection de la faune sauvage. Situé au cœur d’un environnement naturel exceptionnel, la forêt de Brocéliande,  notre zoo offre une multitude de services conçus pour rendre votre visite non seulement agréable, mais aussi éducative et inspirante. Que vous soyez un passionné de la nature, un amateur d'animaux ou une famille en quête d'aventures, nos services sont pensés pour répondre à vos besoins et surpasser vos attentes.</p>
 </section>
 <section class="all-services">
+    <?php
+    $query = $pdo->query('SELECT * FROM service');
+    $services = $query->fetchAll(PDO::FETCH_ASSOC);
+    $i = 0;
+    foreach ($services as $service) { ?>
+    <article class=<?php echo ($i % 2 === 0) ? "left-card" : "right-card"; ?>>
+            <div class="text-services">
+                <h2><?php echo $service['name'] ?></h2>
+                <p><?php echo $service['description'] ?></p>
+            </div>
+            <figure>
+                <img src=<?php echo $service['image_url'] ?> alt=<?php echo $service['image_alt'] ?>>
+            </figure>
+    </article>
+    <?php $i++; 
+    } ?>
+
+
+<!-- 
     <article class="left-card">
         <div class="text-services">
             <h2>Visites guidées</h2>
@@ -65,5 +85,5 @@
         <figure>
             <img src="../../asset/images/services/butterfly-biodiversity-two-column.jpg.thumb.768.768.jpg" alt="Différent papillons">
         </figure>
-    </article>
+    </article> -->
 </section>
