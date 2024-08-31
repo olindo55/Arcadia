2eed7925853a9a22d4cc733534e76a395ff711a0
olindo55 <julien.martinati@gmail.com>
2024-08-31 13:07:51 +0100

    setup the fnc mail

--- asset/pages/contact.php
+++ asset/pages/contact.php
@@ -2,7 +2,7 @@
 
 <form class="container-fluid col-10 col-md-4 py-3 contact-form" >
     <div class="col-10 pb-3">
-        <label for="email" class="form-label">Votre email</label>
+        <label for="email" class="form-label" method="post">Votre email</label>
         <div class="input-group has-validation">
           <span class="input-group-text" id="inputGroupPrepend">@</span>
           <input type="email" class="form-control" id="email" aria-describedby="inputGroupPrepend" required="">
@@ -38,3 +38,21 @@
       <button class="btn btn-primary" id='btnForm' type="button" disabled>Envoyer</button>
     </div>
   </form>
+
+  <!-- for send email -->
+  <?php
+    if (isset($_POST['email']) || isset($_POST['message'])) {
+      $header  = 'MIME-Version: 1.0' . "\r\n";
+      $header .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+      $header .= 'From: emailform@olindo-arcadia.fr' . "\r\n";
+      $header .= 'Reply-to: '. $_POST['email'];
+      
+      $message = '<h1>Message envoyé depuis le formulaire du site web Arcadia</h1>
+      <p><b>Email : </b>' . $_POST['email'] . '<br>
+      <b>Message : </b>' . htmlspecialchars($_POST['message']) . '</p>';
+      
+      $emailToSend = mail('julien.martinati@gmail.com', htmlspecialchars($_POST['subject']), $message, $entete);
+      if($emailToSend)
+            echo '<p>Votre message a bien été envoyé.</p>';
+    }
+    ?>
