97a58057b08a6670b428c7503427eef214fa7429
olindo55 <julien.martinati@gmail.com>
2024-09-11 09:41:01 +0100

    fix css admin button

--- app/Views/templates/button-connection-adm.php
+++ app/Views/templates/button-connection-adm.php
@@ -1,33 +1,36 @@
 <?php if(isset($_SESSION['connected']) && $_SESSION['connected']) : ?>
-    <li class="nav-item dropdown">
-        <button type="button" class="btn btn-secondary">Administration</button>
-        <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-            <span class="visually-hidden">Toggle Dropdown</span>
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <!-- admin -->
-            <?php if(isset($_SESSION['role']) && $_SESSION['role'] === 'administrateur') : ?>
-                <li><a class="dropdown-item <?= $uriParts[1] == 'admUsers' ? 'active' : '' ?>" href="/admUsers/view">Droits et utilisateurs</a></li>
+    <li class="nav-item dropdown" id="admMenu">
+        <h6> </h6>
+        <div>
+            <button type="button" class="btn btn-secondary">Administration</button>
+            <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                <span class="visually-hidden">Toggle Dropdown</span>
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <!-- admin -->
+                <?php if(isset($_SESSION['role']) && $_SESSION['role'] === 'administrateur') : ?>
+                    <li><a class="dropdown-item <?= $uriParts[1] == 'admUsers' ? 'active' : '' ?>" href="/admUsers/view">Droits et utilisateurs</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item <?= $uriParts[1] == 'admServices' ? 'active' : '' ?>" href="/admServices/view">Services</a></li>
+                    <li><a class="dropdown-item <?= $uriParts[1] == 'admComments' ? 'active' : '' ?>" href="/admComments/view">Avis</a></li>
+                    <li><a class="dropdown-item <?= $uriParts[1] == 'admOpening' ? 'active' : '' ?>" href="/admOpening/view">Horaires</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item <?= $uriParts[1] == 'admBiomes' ? 'active' : '' ?>" href="/admBiomes/view">Habitats</a></li>
+                    <li><a class="dropdown-item <?= $uriParts[1] == 'admAnimals' ? 'active' : '' ?>" href="/admAnimals/view">Animaux</a></li>
+                    <li><a class="dropdown-item <?= $uriParts[1] == 'admVet' ? 'active' : '' ?>"a href="/admVet/view">Rapports vétérinaire</a></li>
+                    <li><a class="dropdown-item <?= $uriParts[1] == 'admFeedings' ? 'active' : '' ?>" href="/admFeeding/view">Nourrissages</a></li>
+                <!-- vet -->
+                <?php elseif(isset($_SESSION['role']) && $_SESSION['role'] === 'vétérinaire'): ?>
+                    <li><a class="dropdown-item <?= $uriParts[1] == 'admVet' ? 'active' : '' ?>"a href="/admVet/view">Rapports vétérinaire</a></li>
+                <!-- employee -->
+                <?php elseif(isset($_SESSION['role']) && $_SESSION['role'] === 'employé'): ?>
+                    <li><a class="dropdown-item <?= $uriParts[1] == 'admFeedings' ? 'active' : '' ?>" href="/admFeeding/view">Nourrissages</a></li>
+                    <li><a class="dropdown-item <?= $uriParts[1] == 'admComments' ? 'active' : '' ?>" href="/admComments/view">Avis</a></li>
+                <?php endif; ?>
                 <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item <?= $uriParts[1] == 'admServices' ? 'active' : '' ?>" href="/admServices/view">Services</a></li>
-                <li><a class="dropdown-item <?= $uriParts[1] == 'admComments' ? 'active' : '' ?>" href="/admComments/view">Avis</a></li>
-                <li><a class="dropdown-item <?= $uriParts[1] == 'admOpening' ? 'active' : '' ?>" href="/admOpening/view">Horaires</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item <?= $uriParts[1] == 'admBiomes' ? 'active' : '' ?>" href="/admBiomes/view">Habitats</a></li>
-                <li><a class="dropdown-item <?= $uriParts[1] == 'admAnimals' ? 'active' : '' ?>" href="/admAnimals/view">Animaux</a></li>
-                <li><a class="dropdown-item <?= $uriParts[1] == 'admVet' ? 'active' : '' ?>"a href="/admVet/view">Rapports vétérinaire</a></li>
-                <li><a class="dropdown-item <?= $uriParts[1] == 'admFeedings' ? 'active' : '' ?>" href="/admFeeding/view">Nourrissages</a></li>
-            <!-- vet -->
-            <?php elseif(isset($_SESSION['role']) && $_SESSION['role'] === 'vétérinaire'): ?>
-                <li><a class="dropdown-item <?= $uriParts[1] == 'admVet' ? 'active' : '' ?>"a href="/admVet/view">Rapports vétérinaire</a></li>
-            <!-- employee -->
-            <?php elseif(isset($_SESSION['role']) && $_SESSION['role'] === 'employé'): ?>
-                <li><a class="dropdown-item <?= $uriParts[1] == 'admFeedings' ? 'active' : '' ?>" href="/admFeeding/view">Nourrissages</a></li>
-                <li><a class="dropdown-item <?= $uriParts[1] == 'admComments' ? 'active' : '' ?>" href="/admComments/view">Avis</a></li>
-            <?php endif; ?>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" id="signout" href="/login/signout">Déconnexion</a></li>
-        </ul>
+                <li><a class="dropdown-item" id="signout" href="/login/signout">Déconnexion</a></li>
+            </ul>
+        </div>
         <h6><?php echo $_SESSION['role'] ?></h6>
     </li>
     
