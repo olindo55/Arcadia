4d578af95fe2160873c35aed124c965bf3190d81
olindo55 <30605083+olindo55@users.noreply.github.com>
2024-09-20 18:45:15 +0100

    final modif I hope

--- index.php
+++ index.php
@@ -76,6 +76,7 @@ if (isset($_SESSION['flash_message'])) {
     
     <!-- Bootstrap 5.3.3 -->
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
      <!-- <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
      <link rel="stylesheet" href="/node_modules/bootstrap-icons/font/bootstrap-icons.css"> -->
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
