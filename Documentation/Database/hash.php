5beb308ed5eb40ba4a7c885c83e67135e1fc0344
olindo55 <30605083+olindo55@users.noreply.github.com>
2024-09-21 22:05:47 +0100

    add documentation

--- Documentation/Database/hash.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo password_hash('arcadia123', PASSWORD_BCRYPT);
--- index.php
+++ index.php
@@ -79,9 +79,7 @@ if (isset($_SESSION['flash_message'])) {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">    
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
 
-  
     <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
-
     
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <link rel="stylesheet" href="/asset/scss/style.css">
